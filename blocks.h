ea104f8a4993e5cbf63b08123e60906307cbdcff
Joseph Hines <jthines3@gmail.com>
2021-05-19 21:36:36 -0400

    Updated blocks

--- blocks.h
+++ blocks.h
@@ -1,13 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "sd bar whatsleft",						60,		0},
+	{" ", "sd bar whatsleft",						3600,		0},
 
 	{" ", "sd bar volume",						0,		10},
 
+	{" ", "sd bar traffic",						5,		0},
+
 	{" ", "sd bar battery",						5,		0},
 
-	//{" ", "~/.local/bin/statusbar/temperature",				5,		0},
+	{" ", "sd bar disk",				600,		0},
+
+	{" ", "sd bar cpu_load",				1,		0},
+
+	{" ", "sd bar temperature",				5,		0},
+
+	{" ", "sd bar internet",						30,		0},
 
 	{" ", "sd bar clock",						5,		0},
 
@@ -16,4 +24,4 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char delim = ' ';
